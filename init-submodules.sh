383b902ec39164405f9c1979752a35a51f707a0e
John Merrill <jmerrill@draper.com>
2020-03-20 10:14:04 -0400

    Add script for optionally cloning private submodules

---
+++ init-submodules.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+submodules=($(git submodule--helper list | awk '{$1=$2=$3=""; print substr($0,4)}'))
+
+private_submodules=(
+  pex-kernel
+  pex-firmware
+)
+
+usage () {
+  echo "Usage: ./init-submodules.sh [-h] [-p]"
+  echo "    -h: Display this help message"
+  echo "    -p: Clone additional private modules, which include"
+  echo "    ${private_submodules[@]}"
+}
+
+clone_private=false
+while getopts ":hp" opt; do
+  case "${opt}" in
+    p)
+      clone_private=true
+      ;;
+    h)
+      usage && exit 0
+      ;;
+    ?)
+      usage && exit 1
+      ;;
+  esac
+done
+
+echo ${submodules[@]}
+
+echo ${private_submodules[@]}
+
+if [ $clone_private = false ]; then
+  for module in ${private_submodules[@]}; do
+    echo "Disabling access to $module"
+    git config --local submodule."$module".update none
+  done
+else
+  for module in ${private_submodules[@]}; do
+    echo "Enabling access to $module"
+    git config --local --unset submodule."$module".update
+  done
+fi
+
+git submodule update --init
